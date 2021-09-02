e9a783a77fac2cc7b1f07ab657b76371861d0cec
woodpeckershop <chris.sun5567@gmail.com>
2021-09-02 16:51:30 -0700

    set up appointment

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import "components/Application.scss";
+import Appointment from "components/Appointment";
 
 import DayList from "components/DayList";
 
@@ -22,32 +23,54 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+];
+
 export default function Application() {
   const [day, setDay] = useState("Monday");
   return (
     <main className="layout">
       <section className="sidebar">
-      <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-<DayList
-  days={days}
-  day={day}
-  setDay={setDay}
-/>
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList days={days} day={day} setDay={setDay} />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => {
+          return (
+            <Appointment
+              key={appointment.id}
+              {...appointment} />
+          )
+        })}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,30 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button";
 import InterviewerList from "../InterviewerList";
 
 export default function Form(props) {
-return(
-  <main className="appointment__card appointment__card--create">
-  <section className="appointment__card-left">
-    <form autoComplete="off">
-      <input
-        className="appointment__create-input text--semi-bold"
-        name="name"
-        type="text"
-        placeholder="Enter Student Name"
-        /*
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  // HELPER FUNCTIONS
+  // reset: clears the student name entered in the form and
+  // deselects the currently selected interviewer
+  const reset = function () {
+    setName("");
+    setInterviewer(null);
+  };
+  const cancel = function(){
+    reset()
+    props.onCancel()
+  }
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            /*
           This must be a controlled component
         */
-      />
-    </form>
-    <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
-  </section>
-  <section className="appointment__card-right">
-    <section className="appointment__actions">
-      <Button danger>Cancel</Button>
-      <Button confirm>Save</Button>
-    </section>
-  </section>
-</main>
-)
+          />
+        </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={()=> props.onSave(name,interviewer)}>
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
+  );
 }
